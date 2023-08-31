6fd1b64a124cfba065bf76699033d62e441fe6dc
Subasri-V <subasrivmsg@gmail.com>
2023-08-31 13:22:13 +0530

    added

--- controller/customer.controller.go
+++ controller/customer.controller.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	cus "github.com/Subasri-V/application-new/netxd_customer/netxd"
-	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/Subasri-V/application-new/netxd_customer_dal/interfaces"
 	"github.com/Subasri-V/application-new/netxd_customer_dal/models"
@@ -15,21 +14,51 @@ type RPCServer struct {
 }
 
 var (
-	customerCollection *mongo.Collection
+	//customerCollection *mongo.Collection
 	CustomerDetails interfaces.ICustomer
 )
 
-func (s*RPCServer) CreateCustomer(ctx context.Context,req *cus.CustomerRequest)(*cus.CustomerResponse,error){
-	newCust:=&models.CustomerDetails{Customerid: req.Customerid,Firstname: req.Firstname,Lastname: req.Lastname,Bankid: req.Bankid,Balance: float64(req.Balance),IsActive: req.IsActive}
-	result,err:=CustomerDetails.CreateCustomer(newCust)
+func (s *RPCServer) CreateCustomer(ctx context.Context, req *cus.CustomerRequest) (*cus.CustomerResponse, error) {
+	newCust := &models.CustomerDetails{Customerid: req.Customerid, Firstname: req.Firstname, Lastname: req.Lastname, Bankid: req.Bankid, Balance: int32(req.Balance), IsActive: req.IsActive}
+	result, err := CustomerDetails.CreateCustomer(newCust)
 
-	if err!=nil{
-		return nil,err
+	if err != nil {
+		return nil, err
 	} else {
-		responseCustomer:=&cus.CustomerResponse{
+		responseCustomer := &cus.CustomerResponse{
 			Customerid: result.Customerid,
-	
 		}
-		return responseCustomer,nil
+		return responseCustomer, nil
 	}
+}
+
+func (s*RPCServer) GetCustomerById(ctx context.Context,req *cus.IdReq)(*cus.IdRes,error){
+	//var getCust int32
+	getCust:=req.Customerid
+
+	result,err:=CustomerDetails.GetCustomerById(getCust)
+
+	if err != nil {
+		return nil, err
+	} else {
+		responseCustomer := &cus.IdRes{
+			Customerid: result.Customerid,
+			Firstname: result.Firstname,
+			Balance: result.Balance,
+		}
+		return responseCustomer, nil
+	}
+}
+
+func(s* RPCServer) DeleteCustomerById(ctx context.Context,req *cus.DeleteReq)(*cus.DeleteRes,error){
+	deleteCust:=req.Customerid
+
+	_,err:=CustomerDetails.DeleteCustomerById(deleteCust)
+	if err != nil {
+		return nil, err
+	}
+	return &cus.DeleteRes{
+		Message: "success",
+	},nil
+
 }
--- server/main.go
+++ server/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	cus "github.com/Subasri-V/application-new/netxd_customer/netxd"
-	"github.com/Subasri-V/application-new/netxd_customer_dal/services"
 	"context"
 	"fmt"
 	"net"
+
+	cus "github.com/Subasri-V/application-new/netxd_customer/netxd"
+
 	"github.com/Subasri-V/application-new/netxd_customer_controller/config"
 	"github.com/Subasri-V/application-new/netxd_customer_controller/constants"
 	controllers "github.com/Subasri-V/application-new/netxd_customer_controller/controller"
+	"github.com/Subasri-V/application-new/netxd_customer_dal/services"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"google.golang.org/grpc"
@@ -16,7 +18,7 @@ import (
 
 func initDatabase(client *mongo.Client) {
 	customerCollection := config.GetCollection(client, constants.DatabaseName, "customers")
-	controllers.CustomerDetails= services.InitializeCustomerService(context.Background(),customerCollection)
+	controllers.CustomerDetails = services.InitializeCustomerService(context.Background(), customerCollection)
 }
 func main() {
 	mongoClient, err := config.ConnectDataBase()
